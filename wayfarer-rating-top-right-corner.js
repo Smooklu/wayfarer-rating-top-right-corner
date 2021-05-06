f2860d2299583fecd9d66e62a254561bf5fd59ee
Smooklu <37220586+Smooklu@users.noreply.github.com>
2021-05-05 23:35:35 -0500

    Create wayfarer-rating-top-right-corner.js

---
+++ wayfarer-rating-top-right-corner.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Wayfarer Rating Extra
+// @namespace    https://github.com/Smooklu/wayfarer-rating-top-right-corner
+// @version      0.1
+// @description  Adds your Wayfarer rating in the top right corner.
+// @author       Smooklu
+// @match        https://wayfarer.nianticlabs.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+        var request = new XMLHttpRequest();
+
+        request.addEventListener("load", function(evt){
+            var rating = document.createElement('section');
+            rating.innerText = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
+            rating.className = 'rating-bar__segment rating-bar__segment--active'
+            rating.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
+            var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
+            document.body.children[0].children[2].insertBefore(rating,profilepic)
+        }, false);
+
+        request.open('GET', 'profile', true)
+        request.responseType = "document"
+        request.send();
+
+})();
