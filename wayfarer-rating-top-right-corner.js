f4a13885b0140bbdeeafd26b99dd4e1893699ee1
Smooklu <37220586+Smooklu@users.noreply.github.com>
2021-05-06 09:55:16 -0500

    Update wayfarer-rating-top-right-corner.js

--- wayfarer-rating-top-right-corner.js
+++ wayfarer-rating-top-right-corner.js
@@ -12,13 +12,19 @@
     'use strict';
     var request = new XMLHttpRequest();
     request.addEventListener("load", function(evt){
-        var rating = document.createElement('section');
-        rating.innerText = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
-        rating.className = 'rating-bar__segment rating-bar__segment--active'
-        rating.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
-        rating.title = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].getAttribute('uib-tooltip')
-        var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
-        document.body.children[0].children[2].insertBefore(rating,profilepic)
+        if (request.status == 200) {
+            console.log('Profile loaded!')
+            var rating = document.createElement('section');
+            rating.innerText = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
+            rating.className = 'rating-bar__segment rating-bar__segment--active'
+            rating.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
+            rating.title = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].getAttribute('uib-tooltip')
+            var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
+            document.body.children[0].children[2].insertBefore(rating,profilepic)
+        }
+        else{
+            console.log("Couldn't load profile!")
+        }
     }, false);
 
     request.open('GET', 'profile', true)
