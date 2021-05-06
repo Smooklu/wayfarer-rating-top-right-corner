0de570aeecef9b330ae2dc57f7f3d7679f36b3b2
Smooklu <37220586+Smooklu@users.noreply.github.com>
2021-05-05 23:58:56 -0500

    Update wayfarer-rating-top-right-corner.js

--- wayfarer-rating-top-right-corner.js
+++ wayfarer-rating-top-right-corner.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Wayfarer Rating Extra
+// @name         Wayfarer Rating Top Right Corner
 // @namespace    https://github.com/Smooklu/wayfarer-rating-top-right-corner
 // @version      0.1
 // @description  Adds your Wayfarer rating in the top right corner.
@@ -10,19 +10,19 @@
 
 (function() {
     'use strict';
-        var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", function(evt){
+        var rating = document.createElement('section');
+        rating.innerText = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
+        rating.className = 'rating-bar__segment rating-bar__segment--active'
+        rating.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
+        rating.title = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].getAttribute('uib-tooltip')
+        var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
+        document.body.children[0].children[2].insertBefore(rating,profilepic)
+    }, false);
 
-        request.addEventListener("load", function(evt){
-            var rating = document.createElement('section');
-            rating.innerText = request.response.body.getElementsByClassName('rating-bar__segment--active')[0].innerText;
-            rating.className = 'rating-bar__segment rating-bar__segment--active'
-            rating.style = 'border-radius:20px 20px 20px 20px; text-transform:capitalize; margin-right:10px;'
-            var profilepic = document.getElementsByClassName('inner-container')[1].children[0]
-            document.body.children[0].children[2].insertBefore(rating,profilepic)
-        }, false);
-
-        request.open('GET', 'profile', true)
-        request.responseType = "document"
-        request.send();
+    request.open('GET', 'profile', true)
+    request.responseType = "document"
+    request.send();
 
 })();
